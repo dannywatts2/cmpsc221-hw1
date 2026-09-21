045b24613120cfc6988bd2616a9e9a6544a75048
Danny Watts <312735432+dannywatts2@users.noreply.github.com>
2026-09-21 12:32:51 -0400

    P6: Fixed bug in printChessBoard and began code to fill in remaining queens

--- src/p6_chess/Main.java
+++ src/p6_chess/Main.java
@@ -7,16 +7,42 @@ public class Main {
         // Initialize RNG
         Random rng = new Random();
 
-        // Set up chess board
+        // Create chess board
         boolean[][] queens = new boolean[8][8];
+
+        // Add first queen in a random location
         queens[rng.nextInt(8)][rng.nextInt(8)] = true;
 
+        // Add rest of queens
+        for (int i = 0; i < 7; i++) {
+            boolean placed = false;
+            int j = 0;
+            int k = 0;
+
+            while (!placed) {
+                if (!queens[j][k]) {
+                    queens[j][k] = true;
+                    placed = true;
+                } else {
+                    k++;
+                    if (k >= 8) {
+                        k = 0;
+                        j++;
+                    }
+                    if (j >= 8) {
+                        System.out.println("ERROR");
+                    }
+                }
+            }
+        }
+
+        // Output result to user
         printChessBoard(queens);
     }
 
     private static void printChessBoard(boolean[][] board) {
         for (int i = 0; i < board.length; i++) {
-            System.out.print("| ");
+            System.out.print("|");
             for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j]) {
                     System.out.print("Q");
