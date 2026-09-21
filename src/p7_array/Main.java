13c1f07e2c2e298c5595d9792cc9facc4ed70ed5
Danny Watts <312735432+dannywatts2@users.noreply.github.com>
2026-09-21 17:34:04 -0400

    P7: Updated RNG bound to avoid overflow/underflow errors

--- src/p7_array/Main.java
+++ src/p7_array/Main.java
@@ -15,32 +15,46 @@ public class Main {
         System.out.print("n = ");
         int n = scanner.nextInt();
 
-        // Generate an array of n integers that add up to 0
-        // Algorithm:
-        // 1. Add a random number to the array
-        // 2. Add the negative of the first number to the array
-        // 3. Split index 1 into two numbers that add up to its value and store them in indexes 1 and 2
-        // 4. Repeat this until we reach the final index.
-        // This assumes n >= 2.
-        int[] zeroSum = new int[n];
-        /*for (int i = 0; i < n; i++) {
-            zeroSum[i] = rng.nextInt();
-        }*/
-        zeroSum[0] = rng.nextInt();
-        zeroSum[1] = -zeroSum[0];
+        int[] zeroSum;
+        if (n > 1) {
+            // Generate an array of n integers that add up to 0
+            // Algorithm:
+            // 1. Add a random number to the array
+            // 2. Add the negative of the first number to the array
+            // 3. Split index 1 into two numbers that add up to its value and store them in indexes 1 and 2
+            // 4. Repeat this until we reach the final index.
+            // This assumes n >= 2.
+            zeroSum = new int[n];
+            /*for (int i = 0; i < n; i++) {
+                zeroSum[i] = rng.nextInt();
+            }*/
+            zeroSum[0] = rng.nextInt(n^2);
+            zeroSum[1] = -zeroSum[0];
 
-        for (int i = 1; i < n - 1; i++) {
-            int num = zeroSum[i];
-            zeroSum[i] = rng.nextInt();
-            zeroSum[i + 1] = num - zeroSum[i];
-        }
+            for (int i = 1; i < n - 1; i++) {
+                int num = zeroSum[i];
+                boolean isDuplicate = true;
+                while (isDuplicate) {
+                    zeroSum[i] = rng.nextInt(n ^ 2);
+                    isDuplicate = false;
+                    for (int j = 0; j < i; j++) {
+                        if (zeroSum[i] == zeroSum[j]) {
+                            isDuplicate = true;
+                        }
+                    }
+                }
+                zeroSum[i + 1] = num - zeroSum[i];
+            }
 
-        // Verify result
-        int total = 0;
-        for (int i: zeroSum) {
-            total += i;
+            // Verify result
+            int total = 0;
+            for (int i : zeroSum) {
+                total += i;
+            }
+            // System.out.println(total);
+        } else {
+            zeroSum = new int[] {0};
         }
-        System.out.println(total);
 
         // Output result
         System.out.printf("\n%s", Arrays.toString(zeroSum));
